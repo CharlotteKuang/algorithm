6b5af3a545ed84c83f1a88aa25485a3ab0b395c7
charlotte <charlottekuang430@gmail.com>
2015-05-17 21:25:44 +0800

    [update]add more test cases for 'Integer To Roman'

--- LeetCode/12.IntegerToRoman/main.py
+++ LeetCode/12.IntegerToRoman/main.py
@@ -5,6 +5,8 @@ def main():
 	testCases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 19, 20, 25, 27, 29, 40, 100, 101, 136, 569, 1357, 3999]
 	for test in testCases:
 		print test, solution.intToRoman(test)
+	for i in range(1, 990):
+		print i, solution.intToRoman(i)
 
 if __name__ == '__main__':
 	main()
