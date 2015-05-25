7ad3e1d6890afb7c6cdf846724550b4816b7aef1
charlotte <charlottekuang430@gmail.com>
2015-05-25 19:28:14 +0800

    [update]add explanation to largest rectangle in histogram

--- LeetCode/84.LargestRectangleInHistogram/Solution.py
+++ LeetCode/84.LargestRectangleInHistogram/Solution.py
@@ -1,4 +1,5 @@
 import pdb
+#referenced: http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
 class Solution:
 	# @param {integer[]} height
 	# @return {integer}
@@ -8,6 +9,7 @@ class Solution:
 		maxArea = 0
 		i = 0
         #pdb.set_trace()
+		'''
 		while i < len(height):
 			if len(stack) > 0 and height[stack[-1]] > height[i]:
 				cur = stack.pop(-1)
@@ -18,4 +20,15 @@ class Solution:
 			else: 
 				stack.append(i)
 				i += 1
+		return maxArea
+		'''
+		for i in range(len(height)):
+			if len(stack) > 0 and height[stack[-1]] > height[i]:
+				while len(stack) > 0 and height[stack[-1]] >= height[i]:
+					topIndex = stack.pop(-1) 
+					if len(stack) == 0: width = i	
+					else: width = i-stack[-1]-1 # stack[-1] makes the left index.
+												# there is no element between i and stack[-1] is smaller than height[topIndex]
+					maxArea = max(maxArea, height[topIndex]*width)
+			stack.append(i)
 		return maxArea
