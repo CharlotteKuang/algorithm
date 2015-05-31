489c1ad2330bfec27007e558ce7fbbc1b32036c9
charlotte <charlottekuang430@gmail.com>
2015-05-31 13:00:28 +0800

    [add]Permutation I and II

--- LeetCode/46.Permutation/Solution.py
+++ LeetCode/46.Permutation/Solution.py
@@ -1,9 +1,9 @@
-import copy
-
+import pdb 
 class Solution:
 	def __recursivePermute(self, nums, arr):
 		if len(arr) == len(nums):
-			self.result.append(copy.copy(arr))
+			tmp = [n for n in arr]
+			self.result.append(tmp)
 		else:
 			for i in range(len(nums)):
 				if not self.visited[i]:
@@ -20,3 +20,32 @@ class Solution:
 		self.visited = [False] * len(nums)
 		self.__recursivePermute(nums, [])			
 		return self.result
+
+	def __recursivePermuteUnique(self, nums, arr):
+		#pdb.set_trace()
+		if len(arr) == len(nums):
+			tmp = [n for n in arr]
+			self.result2.append(tmp)
+		else:
+			for i in range(len(nums)):
+				if i > 0 and nums[i] == nums[i-1] and not self.visited2[i-1]:
+					continue
+				if not self.visited2[i]:
+					arr.append(nums[i])
+					self.visited2[i] = True
+					self.__recursivePermuteUnique(nums, arr)
+					arr.pop()
+					self.visited2[i] = False 
+
+	# @param {integer[]} nums
+	# @return {integer[][]}
+	def permuteUnique(self, nums):
+		nums.sort()
+		self.result2 = []
+		self.duplicate = [0] * len(nums)
+		self.visited2 = [False] * len(nums)
+		for i in range(1, len(nums)):
+			if nums[i] == nums[i-1]:
+				self.duplicate[i] = self.duplicate[i-1]+1
+		self.__recursivePermuteUnique(nums, [])			
+		return self.result2
--- LeetCode/46.Permutation/main.py
+++ LeetCode/46.Permutation/main.py
@@ -2,6 +2,9 @@ from Solution import Solution
 
 def main():
 	solution = Solution()
+	print solution.permuteUnique([1,1,1,2])
 	print solution.permute([1,2,3])
+
+	print solution.permuteUnique([1,1,2])
 
 main()
