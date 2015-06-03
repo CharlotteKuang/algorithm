ab5eb2e8979f6d41e8c2cee5c65ee8f8943ce010
charlotte <charlottekuang430@gmail.com>
2015-06-03 11:15:43 +0800

    [update]delete testing code from Solution.py

--- LeetCode/173.BinarySearchTreeIterator/Solution.py
+++ LeetCode/173.BinarySearchTreeIterator/Solution.py
@@ -34,24 +34,4 @@ class BSTIterator:
 					self.__s__.append(cur)
 					cur = cur.left
 			return top.val 
-		else: return None
-
-def iterateTree(node):
-	# Your BSTIterator will be called like this:
-	i, v = BSTIterator(node), []
-	while i.hasNext(): v.append(i.next())
-	print v 
-
-root = TreeNode(6) 
-iterateTree(root)
-
-root.left = TreeNode(3) 
-root.right = TreeNode(7)
-iterateTree(root)
-
-root.left.left = TreeNode(2) 
-root.left.right = TreeNode(4) 
-root.right = TreeNode(8)
-root.right.left = TreeNode(7)
-root.right.right = TreeNode(9)
-iterateTree(root)
+		else: return None 
