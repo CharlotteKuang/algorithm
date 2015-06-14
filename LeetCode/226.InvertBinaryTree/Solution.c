6f0067117c07e696c824c55cd68b56e52260ae82
charlotte <charlottekuang430@gmail.com>
2015-06-14 09:58:37 +0800

    [update] invert tree in C

---
+++ LeetCode/226.InvertBinaryTree/Solution.c
@@ -0,0 +1,16 @@
+/**
+ ** Definition for a binary tree node.
+ ** struct TreeNode {
+ **     int val;
+ **     struct TreeNode *left;
+ **     struct TreeNode *right;
+ ** };
+ **/
+struct TreeNode* invertTree(struct TreeNode* root) {
+	if(root != NULL) {
+		struct TreeNode *tmp = root->left;
+		root->left = invertTree(root->right);
+		root->right = invertTree(tmp);
+    }
+	return root;
+}
