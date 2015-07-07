a07cd7de072dd5ae8989cf4463bd917145085796
charlotte <charlottekuang430@gmail.com>
2015-07-07 20:24:57 +0800

    [update]Solve N-Queens with bit

--- LeetCode/51.NQueens/Solution.py
+++ LeetCode/51.NQueens/Solution.py
@@ -3,27 +3,36 @@ import copy
 
 class Solution:
 	# @return generator
-	def solve(self, num, current):
-		if len(current) == num:
+	def solve(self, n, current):
+		if len(current) == n:
 			board = []
-			for i in range(num):
-				board.append('.' * current[i] + 'Q' + '.' * (num-current[i]-1))
+			for i in range(n):
+				board.append('.' * current[i] + 'Q' + '.' * (n-current[i]-1))
 			yield board 
 		else:
 			row = len(current)
-			for i in range(num): # i for the col candidates of row[len(current)]
+			for col in range(n): # i for the col candidates of row[len(current)]
 				flag = True
-				for j in range(row): #current[j] for col of row[j]
-					if i == current[j] or i == current[j] - j + row or i == j + current[j] - row:
-						flag = False
-						break
+				if self.__colBit & (1 << col): flag = False
+				if self.__45Bit & (1 << (row + col)): flag = False
+				if self.__135Bit & (1 << (n - row + col - 1)): flag = False
 				if flag:
-					for subSolution in self.solve(num, current + [i]):
+					tmp1, tmp2, tmp3 = self.__colBit, self.__45Bit, self.__135Bit
+					self.__colBit |= 1 << col
+					self.__45Bit |= 1 << (row + col)
+					self.__135Bit |= 1 << (n - row + col - 1)
+					for subSolution in self.solve(n, current + [col]):
 						yield subSolution
+					self.__colBit = tmp1
+					self.__45Bit = tmp2
+					self.__135Bit = tmp3
 
 	# @return a list of lists of string
 	def solveNQueens(self, n):
 		result = []
+		self.__colBit = 0
+		self.__45Bit = 0
+		self.__135Bit = 0
 		for solution in self.solve(n, []):		
 			result.append(solution)
 		return result
