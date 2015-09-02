865cea5fdc2170111687465080c5ead369ea9e60
charlotte <charlottekuang430@gmail.com>
2015-09-02 23:13:22 +0800

    O(n) space solve edit distance

--- LeetCode/72.EditDistance/Solution.py
+++ LeetCode/72.EditDistance/Solution.py
@@ -1,5 +1,26 @@
+import pdb
 class Solution(object):
 	def minDistance(self, word1, word2):
+		if word1 == word2: return 0
+		if not word1: return len(word2)
+		if not word2: return len(word1)
+		
+		l1, l2 = len(word1), len(word2)
+		dp = range(l2+1)
+		for i in range(1, 1+l1):
+			for j in range(1+l2):
+				if j == 0:
+					dp_ij = dp[0]
+					dp[0] = i
+				else:
+					dp_ij_bak = dp[j]
+					dp[j] = min(dp[j-1] + 1,
+						dp[j] + 1,
+						dp_ij + (0 if (word1[i-1] == word2[j-1]) else 1))
+					dp_ij = dp_ij_bak
+		return dp[-1]
+				
+	def minDistance_tmp(self, word1, word2):
 		"""
 		:type word1: str
 		:type word2: str
@@ -32,7 +53,8 @@ class Solution(object):
 
 if __name__ == '__main__':
 	sol = Solution()
-	testCases = [('abc', 'acef'), ('a', 'a'), ('abc', 'ddd'), ('abc', '')]
-
+	testCases = [("sea", "ate"), ('abc', 'acef'), ('a', 'a'), ('abc', 'ddd'), ('abc', ''),
+		("sea", "ate")]
+	print sol.minDistance('ab', 'a')
 	for test in testCases:
-		print sol.minDistance(test[0], test[1])
+		print sol.minDistance_tmp(test[0], test[1]), sol.minDistance(test[0], test[1])
