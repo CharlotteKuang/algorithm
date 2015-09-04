203985d4d7b4408a910066ac729798be8641ee53
charlotte <charlottekuang430@gmail.com>
2015-09-05 00:09:23 +0800

    update copy linked list with random pointer

--- LeetCode/138.CopyListWithRandomPointer/Solution.py
+++ LeetCode/138.CopyListWithRandomPointer/Solution.py
@@ -1,12 +1,14 @@
+import pdb
 # Definition for singly-linked list with a random pointer.
-# class RandomListNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.next = None
-#         self.random = None
+class RandomListNode(object):
+	def __init__(self, x):
+		self.label = x
+		self.next = None
+		self.random = None
 
 class Solution(object):
 	def copyRandomList(self, head):
+		pdb.set_trace()
 		"""
 		:type head: RandomListNode
 		:rtype: RandomListNode
@@ -16,7 +18,7 @@ class Solution(object):
 		p = head
 		while p:
 			tmp = p.next
-			p.next = RandomListNode(p.val)
+			p.next = RandomListNode(p.label)
 			p.next.random = p.random
 			p.next.next = tmp
 			p = tmp
@@ -25,10 +27,20 @@ class Solution(object):
 		while p:
 			if p.random:
 				p.next.random = p.random.next
+			p = p.next.next
 
 		p = head
-		rest = head.next
+		rst = head.next
 		while p:
 			new_p = p.next
-			
-		rst = head.next 
+			p.next = new_p.next	
+			if p.next:
+				new_p.next = p.next.next
+			p = p.next
+		return rst
+
+if __name__ == '__main__':
+	node = RandomListNode(1)
+
+	sol = Solution()
+	sol.copyRandomList(node)
