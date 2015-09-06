2844bb2171085ed2aa35ff8a538d8284077a31b3
charlotte <charlottekuang430@gmail.com>
2015-09-06 21:29:52 +0800

    accelerate 130

--- LeetCode/130.SurroundedRegions/Solution.py
+++ LeetCode/130.SurroundedRegions/Solution.py
@@ -11,35 +11,37 @@ class Solution(object):
 
 			def valid(x, y):
 				return 0 <= x < row and 0 <= y < col
-			def is_border(x, y):
-				return 0 == x or x == row-1 or y == 0 or y == col-1
 
 			visited = [[False for n in range(col)] for m in range(row)]
 			directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-			for i in range(row): #1
+			def mark_non_surrounded_region(i, j):
+				q = [(i,j)]
+				visited[i][j] = True
+				board[i][j] = '-'
+				while len(q): #3
+					cur = q.pop(0)
+					for d in directions: #4
+						new_i = cur[0] + d[0]
+						new_j = cur[1] + d[1]
+						if valid(new_i, new_j) and board[new_i][new_j] == 'O' and not visited[new_i][new_j]: #5
+							board[new_i][new_j] = '-'
+							visited[new_i][new_j] = True
+							q.append((new_i, new_j))
+
+			for i in [0, row-1]: #1
 				for j in range(col): #2
-					if board[i][j] != 'O' or visited[i][j] or not is_border(i, j): continue
-					pdb.set_trace()
-					q = [(i,j)]
-					flip = True
-					visited[i][j] = True
-					board[i][j] = '-'
-					while len(q): #3
-						cur = q.pop(0)
-						for d in directions: #4
-							new_i = cur[0] + d[0]
-							new_j = cur[1] + d[1]
-							if valid(new_i, new_j) and board[new_i][new_j] == 'O' and not visited[new_i][new_j]: #5
-								board[new_i][new_j] = '-'
-								visited[new_i][new_j] = True
-								q.append((new_i, new_j))
+					if board[i][j] == 'O' and not visited[i][j]:
+					    mark_non_surrounded_region(i, j)
+			for i in range(row): #1
+				for j in [0, col-1]: #2
+					if board[i][j] == 'O' and not visited[i][j]:
+					    mark_non_surrounded_region(i, j)
 			for i in range(row):
 				for j in range(col):
 					if board[i][j] == '-': board[i][j] = 'O'
 					else: board[i][j] = 'X'
 		except IndexError:
 			pass
-
 if __name__ == '__main__': 
 	sol = Solution()
 	test = [['O', 'O'],['O','O']]
