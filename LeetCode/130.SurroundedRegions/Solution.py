931e0af2313b0cb78b1c377338ae8b54fc25ff73
charlotte <charlottekuang430@gmail.com>
2015-09-06 21:11:43 +0800

    flip the non surrounded regions

--- LeetCode/130.SurroundedRegions/Solution.py
+++ LeetCode/130.SurroundedRegions/Solution.py
@@ -1,3 +1,4 @@
+import pdb
 class Solution(object):
 	def solve(self, board):
 		"""
@@ -17,37 +18,40 @@ class Solution(object):
 			directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 			for i in range(row): #1
 				for j in range(col): #2
-					if board[i][j] != 'O' or visited[i][j]: continue
+					if board[i][j] != 'O' or visited[i][j] or not is_border(i, j): continue
+					pdb.set_trace()
 					q = [(i,j)]
 					flip = True
-					flipCells = [(i, j)]
 					visited[i][j] = True
-					if is_border(i, j): flip = False
+					board[i][j] = '-'
 					while len(q): #3
 						cur = q.pop(0)
 						for d in directions: #4
 							new_i = cur[0] + d[0]
 							new_j = cur[1] + d[1]
 							if valid(new_i, new_j) and board[new_i][new_j] == 'O' and not visited[new_i][new_j]: #5
+								board[new_i][new_j] = '-'
 								visited[new_i][new_j] = True
 								q.append((new_i, new_j))
-								flipCells.append((new_i, new_j))
-								if flip and is_border(new_i, new_j): #6
-									flip = False
-					if flip:
-						for pos in flipCells:
-							board[pos[0]][pos[1]] = 'X'
+			for i in range(row):
+				for j in range(col):
+					if board[i][j] == '-': board[i][j] = 'O'
+					else: board[i][j] = 'X'
 		except IndexError:
 			pass
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
+	sol = Solution()
+	test = [['O', 'O'],['O','O']]
+	sol.solve(test)
+	print test
+
 	test = [
 		['X', 'X', 'X', 'X'],
 		['X', 'O', 'O', 'X'],
 		['X', 'X', 'O', 'X'], 
 		['X', 'O', 'X', 'X'],
 	]
-	sol = Solution()
 
 	sol.solve(test)
 
