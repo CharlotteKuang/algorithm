becdc60d6eaa2e64a37d03a3e5bdfcbe395a57a4
charlotte <charlottekuang430@gmail.com>
2015-11-04 21:12:24 +0800

    [update]Use Binary Search for 300

--- LeetCode/300.LongestIncreasingSubsequence/Solution.py
+++ LeetCode/300.LongestIncreasingSubsequence/Solution.py
@@ -5,21 +5,33 @@ class Solution(object):
 		:type nums: List[int]
 		:rtype: int
 		"""
+		# The more left and small elements are with higher priority to join the 
+		# sub-subsequence
 		if not nums: return 0
 
-		dp = [1]
-		rst = 1
+		def binarySearch(nums, target):
+			head, tail = 0, len(nums)-1
+
+			while head < tail:
+				mid = (tail+head)/2	
+				if target > nums[mid]:
+					head = mid+1
+				else:
+					tail = mid
+			return head if target <= nums[head] else head+1
+
 		pdb.set_trace()
-		for n in range(1, len(nums)):
-			val = 1
-			for m in range(len(dp)-1, -1, -1):
-				if nums[m] < nums[n]:
-					val = max(dp[m]+1, val)
-					rst = max(rst, val)
-			dp.append(val)
-		return rst
+		dp = [nums[0]]
+		for n in nums[1:]:
+			pos = binarySearch(dp, n)
+			if pos >= len(dp):
+				dp.append(n)
+			else:
+				dp[pos] = n 
+		return len(dp) 
 
 if __name__ == '__main__':
 	sol = Solution()
 	print sol.lengthOfLIS([-2, -1])
+	print sol.lengthOfLIS([10,9,2,5,3,7,101,18])
 	print sol.lengthOfLIS([1,3,6,7,9,4,10,5,6])
