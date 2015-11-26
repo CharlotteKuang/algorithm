8febfa0dc9b424443f66b2beeba202e50a06e69f
charlotte <charlottekuang430@gmail.com>
2015-11-26 21:46:09 +0800

    [update] 154 -- I duplicated the array at the last solution, this time I used array referenced

--- LeetCode/154.FindMinimumInRotatedSortedArrayII/Solution.py
+++ LeetCode/154.FindMinimumInRotatedSortedArrayII/Solution.py
@@ -5,17 +5,17 @@ class Solution(object):
         :rtype: int
         """
         # follow your gut
-        if len(nums) == 1: return nums[0]
-
-        head, tail = 0, len(nums)-1
+        #if len(nums) == 1: return nums[0]
+        return self.helper(nums, 0, len(nums)-1)
+    def helper(self, nums, head, tail):
         while head+1 < tail:
             mid = head + (tail-head)/2
             if nums[mid] > nums[tail]:
                     head = mid
             else:
                 if nums[mid] == nums[tail]:
-                    m1 = self.findMin(nums[head:mid+1])
-                    m2 = self.findMin(nums[mid:tail+1])
+                    m1 = self.helper(nums, head, mid)
+                    m2 = self.helper(nums, mid, tail)
                     return min(m1, m2)
                 else:
                     tail = mid
